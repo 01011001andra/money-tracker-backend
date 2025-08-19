4ef170e5f0cb300394134caa3cc9e8044058996a
01011001andra <yandrayt1@gmail.com>
2025-08-20 06:40:53 +0700

    fix

---
+++ src/vercel.main.ts
@@ -0,0 +1,41 @@
+// kalau pakai alias "@/..." aktifkan ini paling awal (opsional kalau semua import relatif)
+import 'tsconfig-paths/register';
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
+
+let server: any; // cache instance antar-invocation
+
+export default async function handler(req: any, res: any) {
+  if (!server) {
+    const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      }),
+    );
+
+    app.setGlobalPrefix('api/v1'); // tanpa leading slash
+
+    app.enableCors({
+      origin: '*',
+      methods: '*',
+      allowedHeaders: '*',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    });
+
+    app.use(json({ limit: '2mb' }));
+    app.use(urlencoded({ limit: '2mb', extended: true }));
+
+    await app.init();
+    server = app.getHttpAdapter().getInstance();
+  }
+
+  return server(req, res);
+}
